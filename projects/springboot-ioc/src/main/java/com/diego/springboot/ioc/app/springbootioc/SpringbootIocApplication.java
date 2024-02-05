3662169b26d8db00a947e03ceae9c4efd52790be
Diego Ludeña <diegolude.martin@gmail.com>
2024-02-05 17:48:14 +0100

    Spring Initializr maven project

---
+++ projects/springboot-ioc/src/main/java/com/diego/springboot/ioc/app/springbootioc/SpringbootIocApplication.java
@@ -0,0 +1,13 @@
+package com.diego.springboot.ioc.app.springbootioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIocApplication.class, args);
+	}
+
+}
---
+++ projects/springboot-ioc/src/test/java/com/diego/springboot/ioc/app/springbootioc/SpringbootIocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.springboot.ioc.app.springbootioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
