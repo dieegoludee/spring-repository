a5befbd2eb6b7be1df7d04ee9e6435f59d62f9e2
Diego Ludeña <diegolude.martin@gmail.com>
2024-02-14 00:37:41 +0100

    Implementando la interfaz WebMvcConfigurer para añadir el interceptor

--- projects/springboot-horario/src/main/java/com/diego/curso/springboot/calendar/interceptor/springboothorario/controllers/AppController.java
+++ projects/springboot-horario/src/main/java/com/diego/curso/springboot/calendar/interceptor/springboothorario/controllers/AppController.java
@@ -8,14 +8,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class AppController {
 
   @GetMapping("/index")
-  public ResponseEntity<?> index() {
+  public ResponseEntity<?> index(HttpServletRequest request) {
     Map<String, Object> data = new HashMap<>();
     data.put("title", "Bienvenidos al sistema de atención");
     data.put("time", new Date());
+    data.put("message", request.getAttribute("message"));
 
     return ResponseEntity.ok(data);
   }
--- projects/springboot-horario/src/main/java/com/diego/curso/springboot/calendar/interceptor/springboothorario/interceptors/CalendarInterceptor.java
+++ projects/springboot-horario/src/main/java/com/diego/curso/springboot/calendar/interceptor/springboothorario/interceptors/CalendarInterceptor.java
@@ -1,8 +1,18 @@
 package com.diego.curso.springboot.calendar.interceptor.springboothorario.interceptors;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,9 +20,50 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class CalendarInterceptor implements HandlerInterceptor {
 
+  private static final Logger logger = LoggerFactory.getLogger(CalendarInterceptor.class);
+
+  @Value("${config.calendar.open}")
+  private Integer open;
+
+  @Value("${config.calendar.close}")
+  private Integer close;
+
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    return true;
+    Calendar calendar = Calendar.getInstance();
+    int hour = calendar.get(Calendar.HOUR_OF_DAY);
+    logger.info("Hour: " + hour);
+
+    if (hour >= open && hour < close) {
+      StringBuilder message = new StringBuilder("Bienvenidos al horario de atención a clientes");
+      message.append(", atendemos desde las ");
+      message.append(open);
+      message.append("hrs. ");
+      message.append("hasta las ");
+      message.append(close);
+      message.append("hrs. ");
+      message.append("Gracias por su visita!");
+
+      request.setAttribute("message", message.toString());
+      return true;
+    }
+
+    // Para crear un JSON personalizado con ObjectMapper
+    ObjectMapper mapper = new ObjectMapper();
+    Map<String, Object> data = new HashMap<>();
+    StringBuilder message = new StringBuilder("Cerrado, fuera del horario de atención ");
+    message.append("por favor visitenos desde las ");
+    message.append(open);
+    message.append(" y las ");
+    message.append(close);
+    data.put("message", message.toString());
+    data.put("date", new Date().toString());
+
+    response.setContentType("application/json");
+    response.setStatus(401);
+    response.getWriter().write(mapper.writeValueAsString(data));
+
+    return false;
   }
 
   @Override
