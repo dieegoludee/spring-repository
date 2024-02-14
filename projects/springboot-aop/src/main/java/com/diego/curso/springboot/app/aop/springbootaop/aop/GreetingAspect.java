0378648f277b497fc0d3c7f97edbd9b03e5d3ff4
Diego Ludeña <diegolude.martin@gmail.com>
2024-02-14 19:44:47 +0100

    Implementando anotación After de org.aspectj.lang.annotation.After

--- projects/springboot-aop/src/main/java/com/diego/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
+++ projects/springboot-aop/src/main/java/com/diego/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
@@ -3,6 +3,7 @@ package com.diego.curso.springboot.app.aop.springbootaop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -18,7 +19,7 @@ public class GreetingAspect {
   // (PointCut) Se ejecuta para devolver el String del método sayHello de la
   // interfaz,
   // con (..) se dice todos los args (regexp)
-  @Before("execution(* com.diego.curso.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))")
+  @Before("execution(* com.diego.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))")
   public void loggerBefore(JoinPoint joinPoint) {
     // Nombre del método
     String method = joinPoint.getSignature().getName();
@@ -26,6 +27,13 @@ public class GreetingAspect {
     String args = Arrays.toString(joinPoint.getArgs());
 
     logger.info("Antes: " + method + " con los argumentos" + args);
+  }
+
+  @After("execution(* com.diego.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))")
+  public void loggerAfter(JoinPoint joinPoint) {
+    String method = joinPoint.getSignature().getName();
+    String args = Arrays.toString(joinPoint.getArgs());
+    logger.info("Despues: " + method + " con los argumentos" + args);
   }
 
 }
