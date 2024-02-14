1775543a167ad42185823a657d8975cd1afb2c6c
Diego Ludeña <diegolude.martin@gmail.com>
2024-02-14 19:02:58 +0100

    Add new project and create Controller

---
+++ projects/springboot-aop/src/main/java/com/diego/curso/springboot/app/aop/springbootaop/controllers/GreetingController.java
@@ -0,0 +1,18 @@
+package com.diego.curso.springboot.app.aop.springbootaop.controllers;
+
+import java.util.Collections;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+  @GetMapping("/greeting")
+  public ResponseEntity<?> greeting() {
+
+    return ResponseEntity.ok(Collections.singletonMap("greeting", null));
+  }
+
+}
