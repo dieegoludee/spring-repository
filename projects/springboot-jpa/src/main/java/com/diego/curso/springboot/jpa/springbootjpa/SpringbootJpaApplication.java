dfcc8843f9fa7dad64946273e204ed4c1d912e03
Diego Ludeña <diegolude.martin@gmail.com>
2024-02-15 13:59:54 +0100

    Implementando la interfaz CommandLineRunner en la clase main y creando la clase Entity Person

--- projects/springboot-jpa/src/main/java/com/diego/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ projects/springboot-jpa/src/main/java/com/diego/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -1,13 +1,19 @@
 package com.diego.curso.springboot.jpa.springbootjpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJpaApplication {
+public class SpringbootJpaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 
 }
