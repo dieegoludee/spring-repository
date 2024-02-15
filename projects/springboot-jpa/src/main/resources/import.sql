7d69243d509b44e668706de2e48420f3e25daa87
Diego Ludeña <diegolude.martin@gmail.com>
2024-02-15 18:14:20 +0100

    Creación de la tabla e inserción de los campos

---
+++ projects/springboot-jpa/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Andrés', 'Guzmán', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Pepe', 'Doe', 'Python');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('María', 'Dow', 'JavaScript');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Marcos', 'Pérez', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Diego', 'Martín', 'Java');
