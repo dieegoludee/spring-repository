83f6409ff30b67c80b7832b422e11b18696466b7
Diego Ludeña <diegolude.martin@gmail.com>
2024-03-06 19:29:42 +0100

    Implementando la anotación JoinColumn

--- projects/springboot-jpa-relationship/src/main/java/com/diego/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
+++ projects/springboot-jpa-relationship/src/main/java/com/diego/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
@@ -1,5 +1,7 @@
 package com.diego.curso.springboot.jpa.springbootjparelationship;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -25,10 +27,11 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		manyToOne();
+		// manyToOneCreate();
+		manyToOneFindByIdClient();
 	}
 
-	public void manyToOne() {
+	public void manyToOneCreate() {
 		Client client = new Client("John", "Doe");
 		clientRepository.save(client);
 
@@ -36,6 +39,20 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 		invoice.setClient(client);
 		Invoice invoiceDB = invoiceRepository.save(invoice);
 		System.out.println(invoiceDB);
+	}
+
+	public void manyToOneFindByIdClient() {
+		Optional<Client> optionalClient = clientRepository.findById(1L);
+
+		if (optionalClient.isPresent()) {
+			Client client = optionalClient.orElseThrow();
+
+			Invoice invoice = new Invoice("Compras de oficina", 2000L);
+			invoice.setClient(client);
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+			System.out.println(invoiceDB);
+		}
+
 	}
 
 }
