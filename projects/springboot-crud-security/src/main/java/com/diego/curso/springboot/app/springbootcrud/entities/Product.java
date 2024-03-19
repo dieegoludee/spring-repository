56897b21516540c982922bf196cada110a88d4e7
Diego Ludeña <diegolude.martin@gmail.com>
2024-03-19 19:07:30 +0100

    Copia del proyecto API RESTful (CRUD) con Spring Security y JWT

---
+++ projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/entities/Product.java
@@ -0,0 +1,82 @@
+package com.diego.curso.springboot.app.springbootcrud.entities;
+
+import com.diego.curso.springboot.app.springbootcrud.validation.IsExistsDb;
+import com.diego.curso.springboot.app.springbootcrud.validation.IsRequired;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+// import jakarta.validation.constraints.NotBlank;
+// import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @IsRequired
+  @IsExistsDb
+  private String sku;
+
+  @Size(min = 3, max = 20) // size min y max del name
+  @IsRequired(message = "{IsRequired.product.name}") // corresponde al messages.properties para mesajes de error
+                                                     // personalizados
+  private String name;
+
+  @NotNull(message = "{NotNull.product.price}")
+  @Min(message = "{Min.product.price}", value = 500) // min price
+  private Integer price;
+
+  @IsRequired // anotación personalizada que valida si el campo es nulo, vacío o con solo
+              // espacios en blanco
+  private String description;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getPrice() {
+    return price;
+  }
+
+  public void setPrice(Integer price) {
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getSku() {
+    return sku;
+  }
+
+  public void setSku(String sku) {
+    this.sku = sku;
+  }
+
+}
---
+++ projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/services/ProductServiceImpl.java
@@ -0,0 +1,75 @@
+package com.diego.curso.springboot.app.springbootcrud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.diego.curso.springboot.app.springbootcrud.entities.Product;
+import com.diego.curso.springboot.app.springbootcrud.repositories.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+  @Autowired
+  private ProductRepository repository;
+
+  @Transactional(readOnly = true)
+  @Override
+  public List<Product> findAll() {
+    return (List<Product>) repository.findAll();
+  }
+
+  @SuppressWarnings("null")
+  @Transactional(readOnly = true)
+  @Override
+  public Optional<Product> findById(Long id) {
+    return repository.findById(id);
+  }
+
+  @SuppressWarnings("null")
+  @Transactional
+  @Override
+  public Product save(Product product) {
+    return repository.save(product);
+  }
+
+  @SuppressWarnings("null")
+  @Transactional
+  @Override
+  public Optional<Product> update(Long id, Product product) {
+    Optional<Product> productOptional = repository.findById(id);
+    if (productOptional.isPresent()) {
+      Product productDb = productOptional.orElseThrow();
+
+      product.setSku(product.getSku());
+      productDb.setName(product.getName());
+      productDb.setPrice(product.getPrice());
+      productDb.setDescription(product.getDescription());
+
+      return Optional.of(repository.save(productDb));
+    }
+    return productOptional;
+  }
+
+  @SuppressWarnings("null")
+  @Transactional
+  @Override
+  public Optional<Product> delete(Long id) {
+    Optional<Product> productOptional = repository.findById(id);
+    productOptional.ifPresent(productDb -> {
+      repository.delete(productDb);
+    });
+
+    return productOptional;
+  }
+
+  @Transactional(readOnly = true)
+  @Override
+  public boolean existsBySku(String sku) {
+    return repository.existsBySku(sku);
+  }
+
+}
