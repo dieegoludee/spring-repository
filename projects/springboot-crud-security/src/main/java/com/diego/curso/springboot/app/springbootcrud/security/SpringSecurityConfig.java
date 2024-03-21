e76064193bd9572fc47b83c5a73ba6e9ee22b425
Diego Ludeña <diegolude.martin@gmail.com>
2024-03-21 20:08:40 +0100

    Implementando Filtro para validar el Token JwtValidationFilter y validando el Token

--- projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/security/SpringSecurityConfig.java
+++ projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/security/SpringSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.diego.curso.springboot.app.springbootcrud.security.filter.JwtAuthenticationFilter;
+import com.diego.curso.springboot.app.springbootcrud.security.filter.JwtValidationFilter;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -37,6 +38,7 @@ public class SpringSecurityConfig {
         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
         .anyRequest().authenticated())
         .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+        .addFilter(new JwtValidationFilter(authenticationManager()))
         .csrf(config -> config.disable())
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
