5885a3970f657cf870fa0f7b5a9148b646704adc
Diego Ludeña <diegolude.martin@gmail.com>
2024-03-21 18:01:36 +0100

    Implementando componente UserDetailsService

--- projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/repositories/UserRepository.java
+++ projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.diego.curso.springboot.app.springbootcrud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.diego.curso.springboot.app.springbootcrud.entities.User;
@@ -7,4 +9,6 @@ import com.diego.curso.springboot.app.springbootcrud.entities.User;
 public interface UserRepository extends CrudRepository<User, Long> {
 
   boolean existsByUsername(String username);
+
+  Optional<User> findByUsername(String username);
 }
