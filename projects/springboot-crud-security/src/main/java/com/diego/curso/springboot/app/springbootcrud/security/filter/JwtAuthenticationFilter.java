969ffebcb2a2a8d6fde85d8851a667d1b820a667
Diego Ludeña <diegolude.martin@gmail.com>
2024-03-21 19:06:40 +0100

    Implementando Login succesful & Generando y firmando el token JWT, Agregando roles en los claims cuendo se genera el token

--- projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/security/filter/JwtAuthenticationFilter.java
+++ projects/springboot-crud-security/src/main/java/com/diego/curso/springboot/app/springbootcrud/security/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.diego.curso.springboot.app.springbootcrud.security.filter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.diego.curso.springboot.app.springbootcrud.entities.User;
@@ -13,8 +18,15 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+// CONSTANTES CON LA SECRET_KEY, HEADER_AUTHORIZATION Y PREFIX_TOKEN
+import static com.diego.curso.springboot.app.springbootcrud.security.TokenJwtConfig.*;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -49,6 +61,37 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         password);
 
     return authenticationManager.authenticate(authenticationToken);
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+      Authentication authResult) throws IOException, ServletException {
+
+    User user = (User) authResult.getPrincipal();
+    String username = user.getUsername();
+    Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+    Claims claims = Jwts.claims().build();
+    claims.put("authorities", roles);
+
+    String token = Jwts.builder()
+        .subject(username)
+        .claims(claims)
+        .expiration(new Date(System.currentTimeMillis() + 3600000)) // fecha de expiración del token
+        .issuedAt(new Date())
+        .signWith(SECRET_KEY)
+        .compact();
+
+    response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+    Map<String, String> body = new HashMap<>();
+    body.put("token", token);
+    body.put("username", username);
+    body.put("message", String.format("Hola %s has iniciado sesión con éxito!", username));
+
+    response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+    response.setContentType("application/json");
+    response.setStatus(200);
+
   }
 
 }
