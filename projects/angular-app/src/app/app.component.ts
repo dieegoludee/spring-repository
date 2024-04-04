236df85dd9f9f705ac220f8085e49a7c6bfd48c9
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-04 18:17:48 +0200

    Modificando los componentes y usando la directiva ngIf

--- projects/angular-app/src/app/app.component.ts
+++ projects/angular-app/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-app';
+  title: string = 'Hola mundo Angular 17';
+  enabled: boolean = false;
+
+  setEnabled(): void {
+    this.enabled = this.enabled ? false : true;
+    console.log('Hemos hecho click en setEnabled');
+  }
 }
