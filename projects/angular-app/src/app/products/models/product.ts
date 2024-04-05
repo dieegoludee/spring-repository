64bca5593f66f56f4fb1aa1672d27d3925a6858e
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-05 13:46:25 +0200

    Añadiendo el componente y el service de Producto

---
+++ projects/angular-app/src/app/products/models/product.ts
@@ -0,0 +1,6 @@
+export class Product {
+  id!: number; // ! significa que luego se va a definir
+  name!: string;
+  description!: string;
+  price!: number;
+}
---
+++ projects/angular-app/src/app/products/services/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Mesa comedor',
+      description: 'Excelente mesa para el comedor',
+      price: 700,
+    },
+    {
+      id: 1,
+      name: 'Teclado mecánico',
+      description: 'Excelente teclado para typing',
+      price: 500,
+    },
+  ];
+
+  constructor() {}
+}
