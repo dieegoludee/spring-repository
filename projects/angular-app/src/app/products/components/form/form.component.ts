f21925a2e89236d70deefbd13eedbb210aee8959
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-11 18:28:57 +0200

    Validando el formulario

--- projects/angular-app/src/app/products/components/form/form.component.ts
+++ projects/angular-app/src/app/products/components/form/form.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'product-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
